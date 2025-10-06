62d435a4caeb050d8a773ce4efc7655a5f0d4aee
Marufa Akter <marufaakter1947@gmail.com>
2025-10-06 23:50:08 +0600

    Added Sorted functionality

--- src/Pages/WishList.jsx
+++ src/Pages/WishList.jsx
@@ -2,27 +2,51 @@ import React, { useEffect, useState } from "react";
 
 const WishList = () => {
   const [wishlist, setWishlist] = useState([]);
+  const [sortOrder,setSortOrder] = useState("none")
   useEffect(() => {
     const savedList = JSON.parse(localStorage.getItem("wishlist"));
     if (savedList) {
       setWishlist(savedList);
     }
   }, []);
+
+const sortedItem = (()=>{
+    if(sortOrder === "price-asc"){
+        return [...wishlist].sort((a,b)=>{
+           return a.price-b.price
+        })
+    }
+    else if(sortOrder === "price-desc"){
+ return [...wishlist].sort((a,b)=>{
+          return  b.price-a.price
+        })
+    }
+    else{
+       return wishlist
+    }
+})()
+
   return (
     <div className="space-y-6">
       <div className="flex justify-between pt-10 items-center  mx-9">
         <h1 className="text-3xl font-semibold">
           Wishlist{" "}
           <span className="text-sm text-gray-500">
-            ({wishlist.length}) Products found.
+            ({sortedItem.length}) Products found.
           </span>
         </h1>
-
-        <button>Sort</button>
+<label className="form-control w-full max-w-xs" >
+    
+       <select className="select select-bordered" value={sortOrder} onChange={e => setSortOrder(e.target.value)}>
+        <option value="none">Sort by price</option>
+        <option value="price-asc">Low-&gt;High</option>
+        <option value="price-desc">High-&gt;Low</option>
+       </select>
+</label>
       </div>
       <div className="space-y-3 px-8 ">
 {
-    wishlist.map(p=><div className="card mb-6 card-side bg-base-100 shadow-sm border ">
+    sortedItem.map(p=><div key={p.id} className="card mb-6 card-side bg-base-100 shadow-sm border ">
   <figure>
     <img
     className="w-40 h-28 object-cover"
