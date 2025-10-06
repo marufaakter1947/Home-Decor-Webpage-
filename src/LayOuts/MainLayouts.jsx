31d0413906d20f7a961bd41ee8af3be4164ba51b
Marufa Akter <marufaakter1947@gmail.com>
2025-10-06 12:27:53 +0600

    Footer added

--- src/LayOuts/MainLayouts.jsx
+++ src/LayOuts/MainLayouts.jsx
@@ -5,9 +5,11 @@ import { Outlet } from 'react-router';
 
 const MainLayouts = () => {
     return (
-        <div>
+        <div className='flex flex-col min-h-screen'>
             <NavBar></NavBar>
+            <div className='flex-1'>
             <Outlet></Outlet>
+            </div>
             <Footer></Footer>
         </div>
     );
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Home from "../Pages/Home";
 import Products from "../Pages/Products";
 import MainLayouts from "../LayOuts/MainLayouts";
 import ErrorPage from "../Pages/ErrorPage";
+import WishList from "../Pages/WishList";
 
 const router = createBrowserRouter([
   {
@@ -12,11 +13,16 @@ const router = createBrowserRouter([
     children:[
    {
     index:true,
-    element: <Home></Home>
+    element: <Home></Home>,
+    loader: ()=> fetch("./furnitureData.json")
   },
   {
     path:"/products",
     Component: Products,
+  },
+  {
+    path:"/wishlist",
+    Component: WishList,
   },
     ]
   },
